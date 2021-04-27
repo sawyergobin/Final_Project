cf2cf14ea618ec65510752a526966fb46e0fae6e
sawyergobin <sawyergobin@outlook.com>
2021-04-27 11:11:13 -0500

    Images optimized, css and js bundled

--- FinalProject.UI.MVC/App_Start/BundleConfig.cs
+++ FinalProject.UI.MVC/App_Start/BundleConfig.cs
@@ -7,24 +7,34 @@ namespace FinalProject.UI.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/Content/scripts").Include(
+                        "~/Content/js/jquery.min.js",
+                        "~/Content/js/jquery.easing.1.3.js",
+                        "~/Content/js/bootstrap.min.js",
+                        "~/Content/js/jquery.waypoints.min.js",
+                        "~/Content/js/owl.carousel.min.js",
+                        "~/Content/js/jquery.magnific-popup.min.js",
+                        "~/Content/js/magnific-popup-options.js",
+                        "~/Content/js/main.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/js/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/styles").Include(
+                      "~/Content/css/animate.css",
+                      "~/Content/css/icomoon.css",
+                      "~/Content/css/themify-icons.css",
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/magnific-popup.css",
+                      "~/Content/css/owl.carousel.min.css",
+                      "~/Content/css/owl.theme.default.min.css",
+                      "~/Content/css/style.css"
+                      ));
         }
     }
 }
